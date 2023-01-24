c28573a5ba06429fb6d0cd1e2b7839481dddce52
Sourabh <Sourabh.Kulkarni@chhj.com>
2023-01-24 12:48:41 +0530

    contact update

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -44,7 +44,7 @@ const Contact = () => {
             />
           </h1>
           <p>
-            I am interested in freelance opportunities - especially on ambitious
+            I am interested in Front-end development opportunities - especially on ambitious
             or large projects. However, if you have any other requests or
             questions, don't hesitate to contact me using below form either.
           </p>
@@ -85,20 +85,20 @@ const Contact = () => {
           </div>
         </div>
         <div className="info-map">
-          Slobodan Gajić,
+          Sourabh Kulkarni,
           <br />
-          Serbia,
+          India,
           <br />
-          Branka RadiČevića 19, 22000 <br />
-          Sremska Mitrovica <br />
+          Siddalingeshwar Colony, Hosur, Hubballi <br />
+          Karnataka <br />
           <br />
-          <span>freelancerslobodan@gmail.com</span>
+          <span>sourabhmk96@gmail.com</span>
         </div>
         <div className="map-wrap">
-          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+          <MapContainer center={[15.3647, 75.1240]} zoom={13}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[44.96366, 19.61045]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            <Marker position={[15.3647, 75.1240]}>
+              <Popup>Sourabh lives here, come over for a cup of coffee :)</Popup>
             </Marker>
           </MapContainer>
         </div>
